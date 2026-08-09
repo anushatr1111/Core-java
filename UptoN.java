e60e20c6bef9cf86ff4dbc0cabdea15430ca9d2c
Anusha T R <anusha@stellix.co.in>
2026-08-09 15:30:48 +0530

    Day 4

---
+++ UptoN.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class UptoN {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+        System.out.println("Numbers from 1 to " + n + ":");
+        for (int i = 1; i <= n; i++) {
+            System.out.print(i + " ");
+        }
+        sc.close();
+    }
+}
